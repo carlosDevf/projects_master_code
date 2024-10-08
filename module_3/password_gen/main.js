885e71c6bc5aff574ce1913a136e223ef6e2f6c9
carlosDevF <carlos@devf.mx>
2024-10-07 19:07:14 -0600

    respuesta del password gen

--- module_3/password_gen/main.js
+++ module_3/password_gen/main.js
@@ -1,1 +1,17 @@
-// Aqui agrega tu código de JavaScript
+const span = document.querySelector("#password");
+span.innerText = "P4S$w0rD";
+
+function generateRandomNumber() {
+  const numbers = "0123456789";
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const specialCharacters = "!@#$%^&*()_+";
+
+  const allCharacters = numbers + letters + specialCharacters;
+  const length = 12;
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters[randomIndex];
+  }
+  span.innerText = password;
+}
